12a973a603abec8d8a96c1796dc25909e0849018
nainoktv <nainoaktvillegas@gmail.com>
2022-10-23 22:02:09 -0700

    feat: add projects to project route with content and links

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ function Home() {
   return (
     <div>
       <h1 className='homeText' id="nainoaText">Nainoa Villegas</h1>
-      <h4 className='homeText' id="seText">Software Engineer</h4>
       <h2 className='homeText' id="portfolioText">Portfolio</h2>
+      <h4 className='homeText' id="seText">Software Engineer</h4>
 
     </div>
   )
--- src/Projects.js
+++ src/Projects.js
@@ -6,34 +6,47 @@ function Projects() {
   return (
     <div>
 
-      <div className="card mb-3" style={{width: '540px'}}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-          <img src={require('./images/hambagu.png')} alt="WaveImage" id="hambaguImg" />
+      <div className="card-group">
+        <div className="card">
+          <img className="card-img-top" src={require('./images/hambagu.png')} alt="WaveImage" id="hambaguImg" />
+          <div className="card-body">
+            <h5 className="card-title"><em>"HAMBAGU 2D Game"</em></h5> 
+            <p className="card-text">Technologies:
+             <br/> 
+             HTML, CSS, and JS
+            </p>
+            <p className="card-text">The purpose of the game is to avoid collision detection by jumping over Burger Aliens approaching the player at random intervals. The goal is to beat your own personal record. Each Alien dodged will add +1 to player score.</p>
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">HAMBAGU</h5>
-              <p className="card-text"><a href="https://github.com/nainoaktv/cyborg-game"  target="_blank" className="repolink">Github Repo</a></p>
-              <p className="card-text"><a href="https://nainoaktv.github.io/cyborg-game/"  target="_blank" className="siteLink">Visit Site Here</a></p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+          <div className="card-footer">
+          <a href="https://github.com/nainoaktv/cyborg-game"  target="_blank" className="repolink">Github Repo</a>
+          <a href="https://nainoaktv.github.io/cyborg-game/"  target="_blank" className="siteLink">Visit Site Here</a>
           </div>
         </div>
-      </div>
 
-      <div className="card mb-3" style={{width: '540px'}}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-          <img src={require('./images/anitrack.png')} alt="WaveImage" id="anitrackImg" />
+        <div className="card">
+          <img className="card-img-top" src={require('./images/anitrack.png')} alt="WaveImage" id="anitrackImg" />
+          <div className="card-body">
+            <h5 className="card-title"><em>"aniTrack"</em></h5>
+            <p className="card-text">Technologies: 
+              <br/> 
+              HTML | CSS | JS | EJS | Bootstrap | PostgreSQL | API
+            </p>
+            <p className="card-text">Users will need to create an account or log in to access the app. Search for an anime with the searchbar on the home screen and add an anime to your user profile's watchlist or click more info which redirects you to MyAnimeList.com for a synopsis. You can also remove anime from your watchlist or leave a comment.</p>
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">aniTrack</h5>
-              <p className="card-text"><a href="https://github.com/nainoaktv/anime-tracker"  target="_blank" className="repolink">Github Repo</a></p>
-              <p className="card-text"><a href="https://anitrack.herokuapp.com/"  target="_blank" className="siteLink">Visit Site Here</a></p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+          <div className="card-footer">
+          <a href="https://github.com/nainoaktv/anime-tracker"  target="_blank" className="repolink">Github Repo</a>
+          <a href="https://anitrack.herokuapp.com/"  target="_blank" className="siteLink">Visit Site Here</a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img className="card-img-top" src={require('./images/comingsoon.jpeg')} alt="Coming Soon" id="comingSoonImg" />
+          <div className="card-body">
+            <h5 className="card-title">Project 3</h5>
+            <p className="card-text">This project is currently in progress.</p>
+          </div>
+          <div className="card-footer">
+            <small className="text-muted">Repo and Site link available soon...</small>
           </div>
         </div>
       </div>

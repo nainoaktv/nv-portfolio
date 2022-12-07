f2031f32efa243d6c9afa89b81e519ce1b421ccc
nainoktv <nainoaktvillegas@gmail.com>
2022-12-06 19:55:18 -0800

    feat: update css and about page.

--- src/About.js
+++ src/About.js
@@ -4,10 +4,12 @@ import './About.css';
 function About() {
 
   return (
-    <div>
+    <div className="about-section">
       <h1>ALOHA,</h1>
-      <p>My name is Nainoa Villegas. Born & Raised in Kapolei, HI</p>
-
+      <p><strong>I'm Nainoa Kaloheaulani Tavake Villegas</strong></p>
+      <p>Born & Raised in Waianae, HI on the island of Oahu</p>
+      <br />
+      <h5><strong><u>Technologies</u></strong></h5>
     </div>
   )
 }

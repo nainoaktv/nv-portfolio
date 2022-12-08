598cd7b810851f4f159fed058280722a74f2f3dc
nainoktv <nainoaktvillegas@gmail.com>
2022-12-08 03:07:15 -0800

    feat: add skill icons to about page

--- src/About.js
+++ src/About.js
@@ -1,15 +1,27 @@
-import './About.css';
-
+import './About.css'
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
 
 function About() {
 
   return (
     <div className="about-section">
-      <h1>ALOHA,</h1>
+      <h1>ALOHA 🤙🏼</h1>
       <p><strong>I'm Nainoa Kaloheaulani Tavake Villegas</strong></p>
       <p>Born & Raised in Waianae, HI on the island of Oahu</p>
       <br />
-      <h5><strong><u>Technologies</u></strong></h5>
+      <h5><strong><u>Skills</u></strong></h5>
+      <FaIcons.FaCss3Alt className='skill_icons' />
+      <FaIcons.FaHtml5 className='skill_icons' />
+      <SiIcons.SiJavascript className='skill_icons' id='js_icon'/>
+      <FaIcons.FaPython className='skill_icons' />
+      <SiIcons.SiMongodb className='skill_icons' id='mongo_icon'/>
+      <SiIcons.SiExpress className='skill_icons' id='express_icon'/>
+      <FaIcons.FaReact className='skill_icons' id='react_icon'/>
+      <FaIcons.FaNodeJs className='skill_icons' id='nodeJS_icon'/>
+      <SiIcons.SiPostgresql className='skill_icons' id='psql_icon'/>
+      <SiIcons.SiDjango className='skill_icons' id='django_icon'/>
+      <SiIcons.SiFlask className='skill_icons' id='flask_icon'/>
     </div>
   )
 }

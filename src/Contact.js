59061fa539cdda570dcac9a3382842f8f9fa4104
nainoktv <nainoaktvillegas@gmail.com>
2022-12-08 17:38:31 -0800

    feat: update about me and create server.js using express

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,14 @@
-
+import React from 'react';
+import './Contact.css';
 
 
 function Contact() {
 
   return (
-    <div>
+    <div className='contact_container'>
       <h1>Contact Me</h1>
-      <p>E-Mail</p>
-      <p>LinkedIn</p>
-      <p>Twitter</p>
+      <p>E-Mail: nainoaktvillegas@gmail.com</p>
+      <a href='https://www.linkedin.com/in/nainoa-villegas/' target="_blank">LinkedIn Link</a>
     </div>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,9 @@ function Home() {
 
   return (
     <div>
-      <h1 className='homeText' id="nainoaText">Nainoa Villegas</h1>
-      <h2 className='homeText' id="portfolioText">Portfolio</h2>
+      <h1 className='homeText' id="nainoaText">Aloha I'm</h1>
+      <h2 className='homeText' id="portfolioText">Nainoa Villegas</h2>
       <h4 className='homeText' id="seText">Software Engineer</h4>
-
     </div>
   )
 }
